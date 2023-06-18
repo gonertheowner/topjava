3334e3db9937fcab2c87ae45db40e1acb0f46926
Kirill Babich <gonertheowner@gmail.com>
2023-06-18 19:14:10 +0300

    fixed InMemory*Test

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -14,8 +14,12 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Autowired
     private UserService service;
+
+    @Autowired
+    public void setService(UserService service) {
+        this.service = service;
+    }
 
     public List<User> getAll() {
         log.info("getAll");

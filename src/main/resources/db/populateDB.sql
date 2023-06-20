2e8ae4c8ccf02de51b330797dc3de20c3f2480ea
Kirill Babich <gonertheowner@gmail.com>
2023-06-20 15:04:51 +0300

    Revert "populateDB.sql + postgres.properties fixes"
    This reverts commit 3743efc766412525775d1081c5264efc3990b257.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -13,10 +13,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
-       ('ADMIN', 100001);
+       ('ADMIN', 100001),
+       ('USER', 100002);
 
 INSERT INTO meals (datetime, description, calories, user_id)
 VALUES ('2020-02-12 10:00:00', 'description', 1000, 100000),
        ('2020-02-15 13:00:00', 'another description', 500, 100000),
-       ('2020-03-16 20:00:00', 'guest meal', 700, 100001),
-       ('2020-03-16 15:30:00', 'another guest meal', 400, 100001);
+       ('2020-03-16 20:00:00', 'guest meal', 700, 100002),
+       ('2020-02-11 15:30:00', 'another guest meal', 400, 100002);

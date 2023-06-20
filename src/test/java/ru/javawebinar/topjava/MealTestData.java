107d90dbb08121ef3f77e6a41aae5be04a05c1df
Kirill Babich <gonertheowner@gmail.com>
2023-06-20 15:20:16 +0300

    fixed meal test data

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,6 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
@@ -15,13 +13,9 @@ public class MealTestData {
     public static final int NOT_FOUND = START_SEQ + 10;
     public static final Meal meal = new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 10, 0),
             "description", 1000);
+    public static final Meal anotherMeal = new Meal(MEAL_ID + 1, LocalDateTime.of(2020, 2, 15, 13, 0),
+            "another description", 500);
     public static final Meal notExistingMeal = new Meal(NOT_FOUND, LocalDateTime.now(), "", 100);
-    public static final List<Meal> meals = Arrays.asList(
-            new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 10, 0),
-                    "description", 1000),
-            new Meal(MEAL_ID + 1, LocalDateTime.of(2020, 2, 15, 13, 0),
-                    "another description", 500)
-    );
 
     public static Meal getNew() {
         return new Meal(null, LocalDateTime.of(2023, 6, 1, 0, 0), "new meal", 500);

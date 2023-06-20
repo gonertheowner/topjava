97660cf5b4dd129983e6520a3d954910b7d21f68
Kirill Babich <gonertheowner@gmail.com>
2023-06-20 12:38:01 +0300

    updated meal test data

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,30 +3,28 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
 
     public static final int MEAL_ID = START_SEQ + 3;
-    public static final int NOT_FOUND = ADMIN_ID;
-    public static final Meal meal = new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 0, 0),
+    public static final int NOT_FOUND = START_SEQ + 10;
+    public static final Meal meal = new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 10, 0),
             "description", 1000);
-    public static final List<Meal> meals = new ArrayList<>();
-
-    static {
-        meals.add(new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 0, 0),
-                "description", 1000));
-        meals.add(new Meal(MEAL_ID + 1, LocalDateTime.of(2020, 1, 12, 0, 0),
-                "another description", 500));
-    }
+    public static final Meal notExistingMeal = new Meal(NOT_FOUND, LocalDateTime.now(), "", 100);
+    public static final List<Meal> meals = Arrays.asList(
+            new Meal(MEAL_ID, LocalDateTime.of(2020, 2, 12, 10, 0),
+                    "description", 1000),
+            new Meal(MEAL_ID + 1, LocalDateTime.of(2020, 2, 12, 13, 0),
+                    "another description", 500)
+    );
 
     public static Meal getNew() {
-        return new Meal(null, LocalDateTime.now(), "new meal", 500);
+        return new Meal(null, LocalDateTime.of(2023, 6, 1, 0, 0), "new meal", 500);
     }
 
     public static Meal getUpdated() {
@@ -37,10 +35,10 @@ public class MealTestData {
     }
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(expected);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).usingRecursiveFieldByFieldElementComparatorIgnoringFields("dateTime").isEqualTo(expected);
+        assertThat(actual).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
     }
 }

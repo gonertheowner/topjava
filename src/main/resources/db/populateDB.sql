57c37832a7332ae994623d1cd2029f1e60d82836
Kirill Babich <gonertheowner@gmail.com>
2023-06-20 15:06:38 +0300

    populateDB.sql + postgres.properties fixes

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -13,11 +13,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
-       ('ADMIN', 100001),
-       ('USER', 100002);
+       ('ADMIN', 100001);
 
 INSERT INTO meals (datetime, description, calories, user_id)
 VALUES ('2020-02-12 10:00:00', 'description', 1000, 100000),
        ('2020-02-15 13:00:00', 'another description', 500, 100000),
-       ('2020-03-16 20:00:00', 'guest meal', 700, 100002),
-       ('2020-02-11 15:30:00', 'another guest meal', 400, 100002);
+       ('2020-03-16 09:00:00', 'admin meal', 700, 100001),
+       ('2020-03-16 22:30:00', 'another admin meal', 400, 100001);
